3c55b08d120b09dc2bfc491224da0a5d0ab40fdc
rnshalinda <nsrhewa@gmail.com>
2025-08-15 10:33:09 +0530

    guide page images added & js image control

--- src/guide.js
+++ src/guide.js
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // else{
     //     getPlantImage(sessionStorage.getItem('plantName'),sessionStorage.getItem('plantType'));
     // }
-    document.querySelector('.plantImage').innerHTML = `<img src="https://pixabay.com/get/gecfae0542494915be00d7ea070790b5ef7ce4fb7ecf4264bae37d08f8807337348ab9a646c89b9abe342848186021636_640.jpg" style="border-radius: 15px; width:360px; height:280px;" alt="">`;
+    document.querySelector('.plantImage').innerHTML = `<img src="https://t1.pixers.pics/img-1fb6f67c/wall-murals-set-fruits-and-vegies.jpg?H4sIAAAAAAAAA3WOW2oDMQxFt2PDZORn7JkF5DdLCMaPdJp5GNttQlZfmdLPIsSVBFfnwtdeXYrg495igW0JYY2QlhW3OpdYl3ckbDBM0RmvK2GM0fn4jsWXI5OTtUNvM4nedH46NG6uPMhHa7nOAFWOeXnhNxRfwW8VBOMG2Bn0pJVTIlkfJnNrLsfmTo-nC8W1Me_3gfWifzEUY4Pq-FaWjWCeA0mNfOY7hX9ovzOgCy5X0BKsAMNhMv10u1y1tMJwhHuppODBRKHFWaJaw5OQMnlpgpvSiJQfRctl9ywBAAA=" style="border-radius: 15px; width:360px; height:280px;" alt="">`;
 });
 
 
@@ -120,10 +120,88 @@ function getPlantImage(plantName, type){
     then(res => res.json()).
     then(data => {
         // console.log(data);
-        
         let imgTag = document.querySelector('.plantImage');
         imgTag.innerHTML = `<img src="${data.hits[0].webformatURL}" style="border-radius: 15px; width:75%; height:65%" alt="">`;
         
     });
 }
 
+
+
+
+
+
+// section scroll background change
+// let backgrounds = [
+//     "url('/assets/images/bg/overview-bg1.jpg')",
+//     "url('/assets/images/bg/overview-bg2.jpg')",
+//     "url('/assets/images/bg/overview-bg3.jpg')",
+//     "url('/assets/images/bg/overview-bg4.jpg')",
+//     "url('/assets/images/bg/overview-bg5.jpg')",
+//     "url('/assets/images/bg/overview-bg6.jpg')",
+//     "url('/assets/images/bg/overview-bg7.jpg')",
+// ];
+
+// const sections = document.querySelectorAll("section");
+// let scrollSections = document.querySelector('.scroll-container');
+
+// scrollSections.addEventListener('scroll', (event) => {
+//     console.log('Window scrolled!');
+//     console.log(scrollSections.scrollTop);
+//     let scrollAmount_fromTop = scrollSections.scrollTop;    // out a certain distance number from top of the page 
+//     let index;
+//     if(scrollAmount_fromTop == 0){
+//         index = 0;
+//     }
+//     if(scrollAmount_fromTop > 0){
+//         index = 1;
+//     }
+//     if(scrollAmount_fromTop > 759){
+//         index = 2;
+//     }
+//     if(scrollAmount_fromTop > 1517){
+//         index = 3;
+//     }
+//     if(scrollAmount_fromTop > 2275){
+//         index = 4;
+//     }
+//     if(scrollAmount_fromTop > 3033){
+//         index = 5;
+//     }
+//     if(scrollAmount_fromTop == 4466){
+//         index = 6;
+//     }
+
+//     document.body.style.setProperty("--bg-image", backgrounds[index]);
+// });
+
+
+let backgrounds = [
+    "url('/assets/images/bg/overview-bg1.webp')",
+    "url('/assets/images/bg/overview-bg2.webp')",
+    "url('/assets/images/bg/overview-bg3.webp')",
+    "url('/assets/images/bg/overview-bg4.webp')",
+    "url('/assets/images/bg/overview-bg5.webp')",
+    "url('/assets/images/bg/overview-bg6.webp')",
+    "url('/assets/images/bg/overview-bg7.webp')",
+];
+
+const sections = document.querySelectorAll(".scroll-container section");
+const scrollContainer = document.querySelector('.scroll-container');
+
+scrollContainer.addEventListener('scroll', () => {
+    let sectionHeight = sections[5].offsetHeight;       // height of one section, each 834px section according to my display size
+    // console.log(sectionHeight);
+    
+    let scrollPos = scrollContainer.scrollTop;          // how far scrolled from top, when in section2 => 834*2 = 1668px
+    // console.log(scrollPos);
+
+    let index = Math.floor(scrollPos / sectionHeight);  // devide total stcrolled amount by section size => get section index
+
+    // Safety: clamp index to last section
+    if (index >= backgrounds.length) index = backgrounds.length - 1;
+    console.log(index);
+    
+    // Change the CSS variable
+    document.body.style.setProperty("--bg-image", backgrounds[index]);
+});
